41de5eef6c2909402821f3f07aee7ef593f3bb4f
Joseph <josephtecson18@gmail.com>
2022-03-30 10:09:54 -0700

    first commit

---
+++ src/test/java/com/training/ers/JenkinsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.ers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
